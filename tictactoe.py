663904d4f1257cead6ef95fe73fe934175e34239
MikeTim <merickson@Millenniumbrooklynhs.org>
2015-02-14 11:23:40 -0500

    Mike finished comments and input

--- tictactoe.py
+++ tictactoe.py
@@ -1,3 +1,13 @@
+########################################
+# Tic - Tac - Toe Game                 #
+# Version 1.0                          #
+########################################
+
+
+
+
+
+# Tic Tac Board Layout |V|
 space = " "
 board = '''
    |   |   
@@ -11,5 +21,36 @@ ___|___|___
    |   |   
 '''
 
-print board%("x", space, space, space, space, space, space, space, space)
+#print board%(space, space, space, space, space, space, space, space, space)
+boardlist = [1,2,3,4,5,6,7,8,9]
+print board%tuple(boardlist)
+player1 = True
+
+
+def replace(n, s):
+	boardlist[n-1] = s
+	return
+
+while True:
+	player = "Player 1"
+	if player1:
+		s = "x"
+	else:
+		player = "Player 2"
+		s = "o"
+	inp = input ("Please specify your desired move...\n> ")
+	if (inp >= 1 and inp <= 9):
+		replace(inp, s)
+		player1 = not player1
+# If you put in a number that breaks it, returns a message |v|
+	else:
+		print ("You have specified an invalid number, please choose a number on the board from 1 to 9")
+
+   	print board%tuple(boardlist)
+
+  
+
+
+
+
 
